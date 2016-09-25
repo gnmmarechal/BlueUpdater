1e9d986be41a2d48734aeb72a20337b2173bc0e6
Gnm Software 2012 <gnmpolicemata91@gmail.com>
2016-09-25 01:22:57 +0100

    Wrote the CFW installation for Luma (thanks, @astronautlevel2 , for the majority of it. It includes support for 6.3+, which requires the arm9 payload for all entrypoints.

--- index-server.lua
+++ index-server.lua
@@ -15,11 +15,6 @@ if System.doesFileExist("/BlueUpdater/settings/usebgm") then
 	usebgm = 1
 end
 
-if System.doesFileExist("/BlueUpdater/settings/keepconfig") then
-	configkeep = 1
-else
-	configkeep = 0
-end
 
 -- Checks
 if usebgm == 1 then
@@ -79,8 +74,7 @@ local payload_path = "/arm9loaderhax.bin"
 -- CFG Paths
 local armcfgpath = "/BlueUpdater/settings/a9lh.cfg"
 local mhxcfgpath = "/BlueUpdater/settings/mhx.cfg"
-local isMenuhax = false
-local menuhaxmode = 1
+local isMenuhax = 0
 
 --System functions
 function fileCopy(input, output)
@@ -100,6 +94,12 @@ function fileCopy(input, output)
 	end
 	io.close(inp)
 	io.close(out)
+end
+
+function sleep(n)
+  local timer = Timer.new()
+  local t0 = Timer.getTime(timer)
+  while Timer.getTime(timer) - t0 <= n do end
 end
 
 function readConfig(fileName)
@@ -196,36 +196,6 @@ function precleanup()
 	end
 end
 
--- Prechecks
-function readconfig(cfgpath, cfw)
-	-- Checks for a config file
-	if System.doesFileExist(cfgpath) then
-		configstream = io.open(cfgpath, FREAD)
-		temppayloadpath = io.read(configstream,0,io.size(configstream))
-		io.close(configstream)
-		if not System.doesFileExist(temppayloadpath) then
-			-- File doesn't exist
-			System.deleteFile(cfgpath)
-			readconfig(cfgpath, cfw)
-		end
-		if cfw == "corbenik" then
-			corbenikarmpayloadpath = temppayloadpath
-		else
-			skeitharmpayloadpath = temppayloadpath
-		end
-	else
-		temppayloadpath = "/arm9loaderhax.bin"
-		if System.doesFileExist("/arm9loaderhax_si.bin") then
-			temppayloadpath = "/arm9loaderhax_si.bin"
-		end
-		if cfw == "corbenik" then
-			corbenikarmpayloadpath = temppayloadpath
-		else
-			skeitharmpayloadpath = temppayloadpath
-		end
-	end
-end
-
 function precheck()
 	--Check model, if N3DS, set clock to 804MHz
 	if System.getModel() == 2 or System.getModel() == 4 then
@@ -234,251 +204,50 @@ function precheck()
 	else
 		newconsole = 0
 	end
-	readconfig(corbenikcfgpath, "corbenik")
-	readconfig(skeithcfgpath, "skeith")
+	readConfig(armcfgpath)
 end
 
-function freshinstall(cfwpath) -- Installs Corbenik/Skeith from scratch
+function installcfw(dlurl)
 	headflip = 1
 	head()
-	-- Lazy fixes
-	Screen.debugPrint(0,180,"B) Quit", black, TOP_SCREEN)
-	if showskeith == 0 then
-		Screen.debugPrint(0,160,"X) Install nightly - Skeith CFW", black, TOP_SCREEN)
-	else
-		Screen.debugPrint(0,160,"X) Update nightly - Skeith CFW", black, TOP_SCREEN)		
-	end	
-	-- Installer
-	if cfwpath == "/corbenik" then
-		cfwname = "Corbenik"
-		cfwurl = corbenikurl
-		armpayloadpath = corbenikarmpayloadpath
-		dl =
-		{
-			native = old.native,
-			nativecetk = old.nativecetk,
-			twl = old.twl,
-			twlcetk = old.twlcetk,
-			agb = old.agb,
-			agbcetk = old.agbcetk
-		}
-	elseif cfwpath == "/skeith" then
-		cfwname = "Skeith"
-		cfwurl = skeithurl
-		armpayloadpath = skeitharmpayloadpath
-		dl =
-		{
-			native = new.native,
-			nativecetk = new.nativecetk,
-			twl = new.twl,
-			twlcetk = new.twlcetk,
-			agb = new.agb,
-			agbcetk = new.agbcetk
-		}
-	end
-	paths =
-	{
-		native = cfwpath.."/lib/firmware/native",
-		nativecetk = cfwpath.."/share/keys/native.cetk",
-		twl = cfwpath.."/lib/firmware/twl",
-		twlcetk = cfwpath.."/share/keys/twl.cetk",
-		agb = cfwpath.."/lib/firmware/agb",
-		agbcetk = cfwpath.."/share/keys/agb.cetk"
-	}	
-	
-	-- Download CFW ZIP
-	debugWrite(0,60,"Downloading "..cfwname.." CFW ZIP...", white, TOP_SCREEN)
+	debugWrite(0,60, "Downloading CFW ZIP...", white, TOP_SCREEN)
 	if updated == 0 then
-		h,m,s = System.getTime()
-		day_value,day,month,year = System.getDate()	
-		Network.downloadFile(cfwurl, localzip)
+        Network.downloadFile(dlurl, localzip)	
 	end
-	
-	-- Extract CFW to its directory
-	debugWrite(0,80,"Extracting CFW files...", white, TOP_SCREEN)
+	debugWrite(0,80, "Backing up payload...", red, TOP_SCREEN)
 	if updated == 0 then
-		-- Renames arm9loaderhax payload to something else to prevent it from being overwritten by the ZIP extraction
-		System.renameFile("/arm9loaderhax.bin", "/arm9loaderhax".."-BACKUP-"..h..m..s..day_value..day..month..year..".bin")
-		System.renameFile("/arm9loaderhax_si.bin", "/arm9loaderhax_si".."-BACKUP-"..h..m..s..day_value..day..month..year..".bin")
-		-- Extracts the CFW's ZIP package
-		System.extractZIP(localzip, "/")
-		-- Deletes the arm9loaderhax payload that was extracted.
-		System.deleteFile("/arm9loaderhax.bin")
-		-- Extracts the payload to its path (according to config or default path)
-		System.extractFromZIP(localzip,"arm9loaderhax.bin",armpayloadpath)
-		-- If default path wasn't one of the standard A9LH paths, rename the previously backed up files to standard.
-		if not System.doesFileExist("/arm9loaderhax.bin") and not System.doesFileExist("/arm9loaderhax_si.bin") and not System.doesFileExist("/homebrew/3ds/boot.bin") then
-			System.renameFile("/arm9loaderhax_si".."-BACKUP-"..h..m..s..day_value..day..month..year..".bin", "/arm9loaderhax_si.bin")
-			System.renameFile("/arm9loaderhax".."-BACKUP-"..h..m..s..day_value..day..month..year..".bin", "/arm9loaderhax.bin")
-		end
-		-- Post-installation cleanup
-		System.deleteFile(localzip)
-		-- Clean files that were in the package and are not needed.
-		if System.doesFileExist("/README.md") then
-			System.deleteFile("/README.md")
-		end
-		if System.doesFileExist("/LICENSE.txt") then
-			System.deleteFile("/LICENSE.txt")
-		end
-		if System.doesFileExist("/generate_localeemu.sh") then
-			System.deleteFile("/generate_localeemu.sh")
-		end
-		if System.doesFileExist("/n3ds_firm.sh") then
-			System.deleteFile("/n3ds_firm.sh")
-		end
-		if System.doesFileExist("/o3ds_firm.sh") then
-			System.deleteFile("/o3ds_firm.sh")
-		end
+        if (System.doesFileExist(backup_path)) then
+            System.deleteFile(backup_path)
+        end
+        if (System.doesFileExist(payload_path)) then
+            System.renameFile(payload_path, backup_path)
+        end
 	end	
-	-- Download FIRM, CETKs, etc.
-	debugWrite(0,100,"Downloading required files...", white, TOP_SCREEN)	
+	debugWrite(0,100, "Installing Luma3DS...", white, TOP_SCREEN)
 	if updated == 0 then
-		Network.downloadFile(dl.native, paths.native)
-		Network.downloadFile(dl.nativecetk, paths.nativecetk)
-		Network.downloadFile(dl.twl, paths.twl)
-		Network.downloadFile(dl.twlcetk, paths.twlcetk)
-		Network.downloadFile(dl.agb, paths.agb)
-		Network.downloadFile(dl.agbcetk, paths.agbcetk)		
-	end	
-	debugWrite(0,120,"Installed. Press A to reboot or B to quit!", green, TOP_SCREEN)
-	updated = 1	
-end
-function installcfw(cfwpath) -- used as "installcfw("/corbenik", 1)", for example, for a Corbenik  installation that keeps old config
-	headflip = 1
-	head()
-	-- Lazy fixes
-	Screen.debugPrint(0,180,"B) Quit", black, TOP_SCREEN)
-	if showskeith == 0 then
-		Screen.debugPrint(0,160,"X) Install nightly - Skeith CFW", black, TOP_SCREEN)
-	else
-		Screen.debugPrint(0,160,"X) Update nightly - Skeith CFW", black, TOP_SCREEN)		
-	end
-	-- Installer
-	if cfwpath == "/corbenik" then
-		cfwname = "Corbenik"
-		cfwurl = corbenikurl
-		armpayloadpath = corbenikarmpayloadpath
-	elseif cfwpath == "/skeith" then
-		cfwname = "Skeith"
-		cfwurl = skeithurl
-		armpayloadpath = skeitharmpayloadpath
-	end
-	-- Check for configkeep variable
-	if configkeep == 1 then
-		keepconfig = 1
+		if isMenuhax == 0 then
+			System.extractFromZIP(localzip, "out/arm9loaderhax.bin", payload_path)
+			System.deleteFile(localzip)
 		else
-		keepconfig = 0
+			System.extractFromZIP(localzip, "out/Luma3DS.dat", "/Luma3DS.dat")
+			if System.doesFileExist("/arm9loaderhax.bin") then
+				System.deleteFile("/arm9loaderhax.bin")
+			end
+			System.extractFromZIP(localzip, "out/arm9loaderhax.bin", "/arm9loaderhax.bin")
+			System.deleteFile(localzip)
+		end	
 	end
-	
-	debugWrite(0,60,"Downloading "..cfwname.." CFW ZIP...", white, TOP_SCREEN)
-	if updated == 0 then -- Download the file
-		Network.downloadFile(cfwurl, localzip)
-	end
-	debugWrite(0,80,"Backing up old installation...", red, TOP_SCREEN)
-	if updated == 0 then -- Back up, and set the back up filename (same filename scheme as the original updater, save for the arm9loaderhax payload)
-		h,m,s = System.getTime()
-		day_value,day,month,year = System.getDate()
-		oldcfwpath = cfwpath.."-BACKUP-"..h..m..s..day_value..day..month..year
-		oldarmpayloadpath = armpayloadpath.."-BACKUP-"..h..m..s..day_value..day..month..year..".bak"
-		System.renameDirectory(cfwpath, oldcfwpath)
-		System.renameFile(armpayloadpath, oldarmpayloadpath)
-	end
-	debugWrite(0,100,"Installing CFW update...", white, TOP_SCREEN)
-	if updated == 0 then
-		-- Renames arm9loaderhax payload to something else to prevent it from being overwritten by the ZIP extraction
-		System.renameFile("/arm9loaderhax.bin", "/arm9loaderhax".."-BACKUP-"..h..m..s..day_value..day..month..year..".bin")
-		System.renameFile("/arm9loaderhax_si.bin", "/arm9loaderhax_si".."-BACKUP-"..h..m..s..day_value..day..month..year..".bin")
-		-- Extracts the CFW's ZIP package
-		System.extractZIP(localzip, "/")
-		-- Deletes the arm9loaderhax payload that was extracted.
-		System.deleteFile("/arm9loaderhax.bin")
-		-- Extracts the payload to its path (according to config or default path)
-		System.extractFromZIP(localzip,"arm9loaderhax.bin",armpayloadpath)
-		-- If default path wasn't one of the standard A9LH paths, rename the previously backed up files to standard.
-		if not System.doesFileExist("/arm9loaderhax.bin") and not System.doesFileExist("/arm9loaderhax_si.bin") and not System.doesFileExist("/homebrew/3ds/boot.bin") then
-			System.renameFile("/arm9loaderhax_si".."-BACKUP-"..h..m..s..day_value..day..month..year..".bin", "/arm9loaderhax_si.bin")
-			System.renameFile("/arm9loaderhax".."-BACKUP-"..h..m..s..day_value..day..month..year..".bin", "/arm9loaderhax.bin")
-		end
-		-- Deletes empty directories that were in the package
-		System.deleteDirectory(cfwpath.."/lib/firmware")
-		System.deleteDirectory(cfwpath.."/share/keys")
-		System.deleteDirectory(cfwpath.."/share/locale/emu")
-		-- Moves required files from old installation to new
-		System.renameDirectory(oldcfwpath.."/lib/firmware",cfwpath.."/lib/firmware")
-		System.renameDirectory(oldcfwpath.."/share/keys",cfwpath.."/share/keys")
-		-- Moves locale files if they exist
-		System.createDirectory(oldcfwpath.."/share/locale/emu")
-		System.renameDirectory(oldcfwpath.."/share/locale/emu",cfwpath.."/share/locale/emu")
-		-- Moves splash screens (top.bin and bottom.bin) to new directory
-		if System.doesFileExist(oldcfwpath.."/share/top.bin") then
-			System.renameFile(oldcfwpath.."/share/top.bin", cfwpath.."/share/top.bin")
-		end
-		if System.doesFileExist(oldcfwpath.."/share/bottom.bin") then
-			System.renameFile(oldcfwpath.."/share/bottom.bin", cfwpath.."/share/bottom.bin")
-		end
-		-- Remove the extra copy of Corbenik if it exists
-		if System.doesFileExist(cfwpath.."/boot/Corbenik") then
-			System.deleteFile(cfwpath.."/boot/Corbenik")
-		end
-		-- Move chainloading payloads to /boot
-		System.deleteDirectory(cfwpath.."/boot")
-		System.createDirectory(oldcfwpath.."/boot")
-		System.renameDirectory(oldcfwpath.."/boot",cfwpath.."/boot")
-		-- Keep config if keepconfig == 1
-		if keepconfig == 1 then
-			-- Deletes the empty /etc directory
-			System.deleteDirectory(cfwpath.."/etc")
-			-- Moves cache and config to installation directory
-			System.renameDirectory(oldcfwpath.."/etc",cfwpath.."/etc")
-			System.renameDirectory(oldcfwpath.."/var/cache",cfwpath.."/var/cache")
-		end
-		-- Post-installation cleanup
-		System.deleteFile(localzip)
-		-- Clean files that were in the package and are not needed.
-		if System.doesFileExist("/README.md") then
-			System.deleteFile("/README.md")
-		end
-		if System.doesFileExist("/LICENSE.txt") then
-			System.deleteFile("/LICENSE.txt")
-		end
-		if System.doesFileExist("/generate_localeemu.sh") then
-			System.deleteFile("/generate_localeemu.sh")
-		end
-		if System.doesFileExist("/n3ds_firm.sh") then
-			System.deleteFile("/n3ds_firm.sh")
-		end
-		if System.doesFileExist("/o3ds_firm.sh") then
-			System.deleteFile("/o3ds_firm.sh")
+	if isMenuhax == 0 then
+		debugWrite(0, 120, "Changing path for reboot patch...", red, TOP_SCREEN)
+		if updated == 0 then
+			path_changer()
 		end
 	end
-	debugWrite(0,120,"Updated. Press A to reboot or B to quit!", green, TOP_SCREEN)
+	debugWrite(0,140,"Updated. Press A to reboot or B to quit!", green, TOP_SCREEN)
 	updated = 1	
 end
 
-function isdirtyupdate() -- Checks whether to keep config or not and sets the var for it.
-	if configkeep == 1 then
-		configkept = "Yes"
-	else
-		configkept = "No"
-	end
-	if Controls.check(pad, KEY_R) and not Controls.check(oldpad, KEY_R) then
-		if configkeep == 1 then
-			configkeep = 0
-			keepconfig = 0
-			-- Delete config setting for this option
-			System.deleteFile("/corbenik-updater-re/settings/keepconfig")
-		else
-			configkeep = 1
-			keepconfig = 1
-			-- Create config option for this option to be saved upon exit and restart
-			confsettingstream = io.open("/corbenik-updater-re/settings/keepconfig",FCREATE)
-			io.write(confsettingstream,0,"Keep Config", 11)
-			io.close(confsettingstream)
-		end
-	end
-end
-
-function bgmtogglecheck() -- Checks for KEY_L and toggles BGM usage (requires restart of the updater to take effect)
+function bgmtogglecheck() -- Checks for KEY_SELECT and toggles BGM usage (requires restart of the updater to take effect)
 	if Controls.check(pad, KEY_SELECT) and not Controls.check(oldpad, KEY_SELECT) then
 		if System.doesFileExist("/corbenik-updater-re/settings/usebgm") then
 			System.deleteFile("/corbenik-updater-re/settings/usebgm")
@@ -489,21 +258,64 @@ function bgmtogglecheck() -- Checks for KEY_L and toggles BGM usage (requires re
 		end
 	end
 end
+
+
+function checkmenuhaxmode() -- Checks whether to keep config or not and sets the var for it.
+
+	if isMenuhax == 1 then
+
+		modename = "MenuHax"
+
+	else
+
+		modename = "Arm9LoaderHax"
+
+	end
+
+	if Controls.check(pad, KEY_R) and not Controls.check(oldpad, KEY_R) then
+
+		if isMenuhax == 1 then
+
+			isMenuhax = 0
+
+			-- Delete config setting for this option
+
+			System.deleteFile("/BlueUpdater/settings/menuhax")
+
+		else
+			
+			isMenuhax = 1
+
+			-- Create config option for this option to be saved upon exit and restart
+
+			confsettingstream = io.open("/BlueUpdater/settings/menuhax",FCREATE)
+
+			io.write(confsettingstream,0,"Keep Config", 11)
+
+			io.close(confsettingstream)
+
+		end
+
+	end
+
+end
+
+
 -- Actual UI screens
 
 function head() -- Head of all screens
 	if headflip == 1 then
-		debugWrite(0,0,"Corbenik CFW Updater: RE v."..version, white, TOP_SCREEN)
+		debugWrite(0,0,"BlueUpdater v."..version, white, TOP_SCREEN)
 		debugWrite(0,20,"==============================", red, TOP_SCREEN)	
 	end
-	Screen.debugPrint(0,0,"Corbenik CFW Updater: RE v."..version, white, TOP_SCREEN)
+	Screen.debugPrint(0,0,"BlueUpdater v."..version, white, TOP_SCREEN)
 	Screen.debugPrint(0,20,"==============================", red, TOP_SCREEN)	
 end
 
 function bottomscreen() -- Bottom Screen
 	if headflip == 1 then
-		debugWrite(0,0, "Latest Corbenik CFW: v"..corbenikver, green, BOTTOM_SCREEN)
-		debugWrite(0,20, "Latest Skeith CFW: "..skeithver, green, BOTTOM_SCREEN)
+		debugWrite(0,0, "Latest stable: v"..stablever, green, BOTTOM_SCREEN)
+		debugWrite(0,20, "Latest hourly: "..nightlyver, green, BOTTOM_SCREEN)
 		debugWrite(0,40, "==============================", red, BOTTOM_SCREEN)
 		debugWrite(0,60, "CURE Version: "..version, white, BOTTOM_SCREEN)
 		debugWrite(0,80, "CORE Version: "..bootstrapver, white, BOTTOM_SCREEN)
@@ -511,11 +323,11 @@ function bottomscreen() -- Bottom Screen
 		debugWrite(0,120, "Author: gnmmarechal", white, BOTTOM_SCREEN)
 		debugWrite(0,140, "Special Thanks:", white, BOTTOM_SCREEN)
 		debugWrite(0,160, "Crystal the Glaceon (Tester)", white, BOTTOM_SCREEN)
-		debugWrite(0,180, "chaoskagami (CFW Developer)", white, BOTTOM_SCREEN)
+		debugWrite(0,180, "astronautlevel (StarUpdater)", white, BOTTOM_SCREEN)
 		debugWrite(0,200, "Rinnegatamante (LPP-3DS/Help)", white, BOTTOM_SCREEN)
 	end
-	Screen.debugPrint(0,0, "Latest Corbenik CFW: v"..corbenikver, green, BOTTOM_SCREEN)
-	Screen.debugPrint(0,20, "Latest Skeith CFW: "..skeithver, green, BOTTOM_SCREEN)
+	Screen.debugPrint(0,0, "Latest stable: v"..stablever, green, BOTTOM_SCREEN)
+	Screen.debugPrint(0,20, "Latest hourly: "..nightlyver, green, BOTTOM_SCREEN)
 	Screen.debugPrint(0,40, "==============================", red, BOTTOM_SCREEN)
 	Screen.debugPrint(0,60, "CURE Version: "..version, white, BOTTOM_SCREEN)
 	Screen.debugPrint(0,80, "CORE Version: "..bootstrapver, white, BOTTOM_SCREEN)	
@@ -523,48 +335,31 @@ function bottomscreen() -- Bottom Screen
 	Screen.debugPrint(0,120, "Author: gnmmarechal", white, BOTTOM_SCREEN)
 	Screen.debugPrint(0,140, "Special Thanks:", white, BOTTOM_SCREEN)
 	Screen.debugPrint(0,160, "Crystal the Glaceon (Tester)", white, BOTTOM_SCREEN)
-	Screen.debugPrint(0,180, "chaoskagami (CFW Developer)", white, BOTTOM_SCREEN)
+	Screen.debugPrint(0,180, "astronautlevel (StarUpdater)", white, BOTTOM_SCREEN)
 	Screen.debugPrint(0,200, "Rinnegatamante (LPP-3DS/Help)", white, BOTTOM_SCREEN)	
 end
 
 function firstscreen() -- scr == 1 | First UI screen, main menu
 	head()
-	Screen.debugPrint(0,40,"Welcome to Corbenik CFW Updater: RE!", white, TOP_SCREEN)
+	Screen.debugPrint(0,40,"Welcome to BlueUpdater!", white, TOP_SCREEN)
 	Screen.debugPrint(0,100,"Please select an option:", white, TOP_SCREEN)
-	Screen.debugPrint(0,120,"Keep Config (Press R): "..configkept, white, TOP_SCREEN)
-	if showcorbenik == 1 then
-		Screen.debugPrint(0, 140,"A) Update stable - Corbenik CFW", white, TOP_SCREEN)
-		inputscr(2, KEY_A)
-	else
-		Screen.debugPrint(0, 140,"A) Install stable - Corbenik CFW", white, TOP_SCREEN)
-		inputscr(4, KEY_A)
-	end
-	if showskeith == 1 then
-		Screen.debugPrint(0,160,"X) Update nightly - Skeith CFW", white, TOP_SCREEN)
-		inputscr(3, KEY_X)
-	else
-		Screen.debugPrint(0, 160,"X) Install nightly - Skeith CFW", white, TOP_SCREEN)
-		inputscr(5, KEY_X)		
-	end
+	Screen.debugPrint(0,120,"Mode (Press R): "..modename, white, TOP_SCREEN)
+	Screen.debugPrint(0, 140,"A) Update stable Luma3DS", white, TOP_SCREEN)
+	inputscr(2, KEY_A)
+
+	Screen.debugPrint(0, 160,"X) Update hourly Luma3DS", white, TOP_SCREEN)
+	inputscr(3, KEY_X)
 	Screen.debugPrint(0,180,"B) Quit", white, TOP_SCREEN)
 	inputscr(-1, KEY_B)
 end
 
-function installer(cfwpath) -- scr == 2/3 | Installation UI screen
+function installer(dlurl) -- scr == 2/3 | Installation UI screen
 	head()
 	debugWrite(0, 40, "Started installation of CFW...", white, TOP_SCREEN)
-	installcfw(cfwpath)
+	installcfw(dlurl)
 	inputscr(-1, KEY_B) -- Checks for exit
 	inputscr(-2, KEY_A) -- Checks for reboot
 end
-
-function newinstaller(cfwpath) -- scr = 4/5 | Installation UI Screen (FRESH)
-	head()
-	debugWrite(0, 40, "Started fresh installation of CFW...", white, TOP_SCREEN)
-	freshinstall(cfwpath)
-	inputscr(-1, KEY_B)
-	inputscr(-2, KEY_A)
-end	
 
 -- Main Loop
 precheck()
@@ -574,19 +369,13 @@ while true do
 	clear()
 	pad = Controls.read()
 	bottomscreen() -- Display bottom screen info
-	-- Checks for BGM toggle and dirty/clean update toggle
-	isdirtyupdate()
 	bgmtogglecheck()
 	-- Actual UI screens and installer phases
 	
 	if scr == 3 then
-		installer("/skeith")
+		installer(url.hourly)
 	elseif scr == 2 then
-		installer("/corbenik")
-	elseif scr == 4 then
-		newinstaller("/corbenik")
-	elseif scr == 5 then
-		newinstaller("/skeith")
+		installer(url.stable)
 	elseif scr == 1 then
 		firstscreen()
 	end
